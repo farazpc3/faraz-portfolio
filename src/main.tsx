bcded647f768bd6af5303b909f9b2cf8f8f55712
farazpc3 <farazpc3@gmail.com>
2024-12-11 21:40:47 +0330

    added main bootstrap navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
-import 
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
