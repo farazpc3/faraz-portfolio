bc8ba709dddeeaaf56def8c9b066c7d3280327c9
farazpc3 <farazpc3@gmail.com>
2024-12-13 13:54:39 +0330

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,18 @@ function App() {
 
   return (
     <>
-      <header>
+      <header className="main-header">
+        <h1>Welcome to my portfolio</h1>
         <MainNavBar />
       </header>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <aside className="left-side-bar"></aside>
+      <main className="main">
+        <header></header>
+      </main>
+      <aside className="right-side-bar"></aside>
+      <footer className="main-footer">
+        <p>Copyright &copy; 2023 My Portfolio</p>
+      </footer>
     </>
   );
 }
