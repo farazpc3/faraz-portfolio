d98f59bbb90439a56a7457a77b45e25c37707967
farazpc3 <farazpc3@gmail.com>
2024-12-14 21:51:13 +0330

    adding responsive navbar instead for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import MainNavBar from "./components/MainNavBar.tsx";
+import MainNavBar from "./components/MainNavBar";
 
 function App() {
   const [count, setCount] = useState(0);
